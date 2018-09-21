de60f5b05ac807065dc33c230592ab9ace7923e4
Eric Lopez <e_lo28@yahoo.com>
2018-09-20 21:39:52 -0700

    add w5d4

---
+++ w5d4/madlib.js
@@ -0,0 +1,6 @@
+function madLib(verb, adjective, noun){
+  
+}
+
+madLib('make', 'best', 'guac');
+// => "We shall MAKE the BEST GUAC."
---
+++ w5d4/mysteryscoping.js
@@ -0,0 +1,55 @@
+function mysteryScoping1() {
+  var x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// mysteryScoping1();
+// prints in block, in block
+
+function mysteryScoping2() {
+  const x = 'out of block';
+  if (true) {
+    const x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// mysteryScoping2();
+// prints in block, out of block
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// mysteryScoping3();
+// throws an error, Identifier 'x' has already been declared
+
+function mysteryScoping4() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// mysteryScoping4();
+// prints in block, out of block
+
+function mysteryScoping5() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  let x = 'out of block again';
+  console.log(x);
+}
+// mysteryScoping5();
+// throws an error, Identifier 'x' has already been declared
